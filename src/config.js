3156377a6531b09360ee3460a134edea199bc162
Romario Santos <romariojosesantosaz@gmail.com>
2024-10-04 11:46:10 -0300

    sdfsdf

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://d4b6-131-161-65-60.ngrok-free.app",
     apiKey: "0417bf43b0a8969bd6685bcb49d783df",
     key:'87F3F7D0-4B8A-45D0-8618-7399E4AD646',
     instanceName: "ZXCV",
